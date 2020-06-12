85b2c66498cc25b67e7cf3c3caabe660d44db4a1
LiudasBu <44939049+LiudasBu@users.noreply.github.com>
2020-06-12 11:12:43 +0300

    added Trip class

---
+++ Keliones/Trip.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Keliones
+{
+    public class Trip
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string PradziosData { get; set; }
+        public string PabaigosData { get; set; }
+        public string Salis { get; set; }
+        public List<string> Miestai { get; set; }
+        public decimal Rating { get; set; }
+
+    }
+}
