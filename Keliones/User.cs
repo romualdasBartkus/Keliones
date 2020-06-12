e744e905845d4fb466f1933351707e747fbc73cb
RBartkus <34437319+RBartkus@users.noreply.github.com>
2020-06-12 11:42:40 +0300

    User class

---
+++ Keliones/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Keliones
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public List<Trip> UserTrips { get; set; }
+        public string UserCountry { get; set; }
+    }
+}
