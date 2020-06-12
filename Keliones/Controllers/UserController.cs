897f2bfe80132369d4ddf1810dd124db94023e7e
LiudasBu <44939049+LiudasBu@users.noreply.github.com>
2020-06-12 14:38:45 +0300

    added get methods for user class

---
+++ Keliones/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Keliones.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // GET: api/User
+        [HttpGet]
+        public List<User> Get()
+        {
+            return DbContext.GetUser();
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "Get")]
+        public User Get(int id)
+        {
+            List<User> users = DbContext.GetUser();
+            foreach (User u in users) if (u.ID == id) return u;
+
+            //null jei neranda userio, reikia pakeist, kad mestu errora
+            return null;
+
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
