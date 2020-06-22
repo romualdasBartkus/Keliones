9674b9b7d07ea1e869d8c586aea14dfc99539b1f
romualdasBartkus <66728554+romualdasBartkus@users.noreply.github.com>
2020-06-22 11:33:07 +0300

    tripControler, DBContext

---
+++ TripController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace Keliones.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripController : ControllerBase
+    {
+        // GET: api/Trip
+        [HttpGet]
+        public List<Trip> Get()
+        {
+            return DbContext.GetTrips();
+        }
+
+        // GET api/Trip/5
+        [HttpGet("{id}")]
+        public Trip Get(int id)
+        {
+            return DbContext.GetTrip(id);
+        }
+
+        // POST api/Trip
+        [HttpPost]
+        public void Post([FromBody] Trip trip)
+        {
+            DbContext.InsertTrip(trip);
+        }
+
+        // PUT api/Trip/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Trip trip)
+        {
+          // 
+        }
+
+        // DELETE api/Trip/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            DbContext.DeleteTrip(id);
+        }
+    }
+}
